9315d483c02ce1ed1f8b0fb29ab43e689f55f1a4
Pavel Studeník <pavel.studenik@kiwi.com>
2021-05-07 06:53:44 +0200

    Show fps in example app

--- example.py
+++ example.py
@@ -1,10 +1,17 @@
+from time import time
 import freenect
 
 
 def main():
+    timestamp = int(time())
+    fps = 0
+
     while True:
        image, _ = freenect.sync_get_video()
-       print(image.shape)
+       if int(time()) != timestamp:
+          print(image.shape, "fps:", fps)
+          fps, timestamp = 0, int(time())
+       fps += 1
 
-if __name__=="__main__"
+if __name__=="__main__":
     main()
